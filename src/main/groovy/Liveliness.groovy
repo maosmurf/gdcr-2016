d525d3e706f22311c9dd18e68f3ee4097611b881
Adam Zielinski <maosmurf@gmail.com>
2016-10-22 14:12:16 +0200

    specs in NeighbourCount.groovy

--- src/main/groovy/Liveliness.groovy
+++ src/main/groovy/Liveliness.groovy
@@ -1,17 +1,8 @@
-import static NeighbourCount.THREE
-import static NeighbourCount.TWO
-
 enum Liveliness {
-    ALIVE{
-        def next(NeighbourCount neighbours) {
-            (neighbours == TWO || neighbours == THREE) ? this : DEAD
-        }
-    },
-    DEAD{
-        def next(NeighbourCount neighbours) {
-            neighbours != THREE ? this : ALIVE
-        }
-    };
+    ALIVE,
+    DEAD
 
-    abstract def next(NeighbourCount neighbours);
+    def next(NeighbourCount neighbours) {
+        neighbours.next(this)
+    }
 }
--- src/main/groovy/NeighbourCount.groovy
+++ src/main/groovy/NeighbourCount.groovy
@@ -1,6 +1,27 @@
+import static Liveliness.ALIVE
+import static Liveliness.DEAD
+
 enum NeighbourCount {
-    LESS_THAN_TWO,
-    TWO,
-    THREE,
-    MORE_THAN_THREE
+    LESS_THAN_TWO{
+        def next(liveliness) {
+            DEAD
+        }
+    },
+    TWO{
+        def next(liveliness) {
+            liveliness
+        }
+    },
+    THREE{
+        def next(liveliness) {
+            ALIVE
+        }
+    },
+    MORE_THAN_THREE{
+        def next(liveliness) {
+            DEAD
+        }
+    }
+
+    abstract def next(liveliness)
 }
